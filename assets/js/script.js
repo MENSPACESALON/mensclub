811df8a4b36d7f9985da699fc1bc0a5bd2ac9d3a
DaviFrt <daviamomfb@gmail.com>
2023-05-01 15:07:29 -0300

    Finishes the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-const testimonialsSlider = document.querySelector(".testimonials-slider");
+function initMenu() {
+  const buttonMenu = document.querySelector(".btn-mobile");
+  const links = document.querySelectorAll("nav ul li a");
 
-const flkty = new Flickity(testimonialsSlider, {
-  cellAlign: "center",
-  contain: true,
-  groupCells: "80%",
-  autoPlay: 2500,
-});
+  function toggleMenu() {
+    const nav = document.querySelector("header nav");
+    nav.classList.toggle("active");
+  }
+
+  buttonMenu.addEventListener("click", toggleMenu);
+  links.forEach((link) => {
+    link.addEventListener("click", toggleMenu);
+  });
+}
+
+function initSlider() {
+  const testimonialsSlider = document.querySelector(".testimonials-slider");
+
+  const flkty = new Flickity(testimonialsSlider, {
+    cellAlign: "center",
+    contain: true,
+    groupCells: "80%",
+    autoPlay: 2500,
+  });
+}
+
+initMenu();
+initSlider();
